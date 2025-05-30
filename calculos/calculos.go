c77c36ddccea7ef5c060e8ecac9c5adfd4106c61
Thiago Fernandes <thiago.eu.2017@gmail.com>
2025-05-30 11:50:19 -0300

    s

--- calculos/calculos.go
+++ calculos/calculos.go
@@ -2,21 +2,17 @@ package calculos
 
 // calcular espessura da laje
 func Espessura(lx, ly, cobrimento, engaste float64) float64 {
-	var l float64
+	l := lx
 
 	if ly*0.7 < lx {
 		l = ly * 0.7
-		return l
-	} else {
-		l = lx
-		return l
 	}
 
-	d := 2.5 - (0.1 * engaste) * l
+	d := 2.5 - (0.1*engaste)*l
 
-	H := d + 0.5 + cobrimento
+	h := d + 0.5 + cobrimento
 
-	return H
+	return h
 }
 
 func Lambda(lx, ly float64) float64 {
