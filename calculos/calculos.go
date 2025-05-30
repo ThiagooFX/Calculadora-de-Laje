6a1baf5af86e1d2627d1171ddf9a21b7f1102efe
Thiago Fernandes <thiago.eu.2017@gmail.com>
2025-05-30 11:38:00 -0300

    funçãopara calcular espessura

--- calculos/calculos.go
+++ calculos/calculos.go
@@ -1,16 +1,35 @@
 package calculos
 
-func Lambda(x, y float64) float64 {
-    return x / y
+// calcular espessura da laje
+func Espessura(lx, ly, cobrimento, engaste float64) float64 {
+	var l float64
+
+	if ly*0.7 < lx {
+		l = ly * 0.7
+		return l
+	} else {
+		l = lx
+		return l
+	}
+
+	d := 2.5 - (0.1 * engaste) * l
+
+	H := d + 0.5 + cobrimento
+
+	return H
 }
 
-func PesoProprio(gama, espessura float64) float64{	//Peso Próprio
+func Lambda(lx, ly float64) float64 {
+	return lx / ly
+}
+
+func PesoProprio(gama, espessura float64) float64 { //Peso Próprio
 	return gama * espessura
 }
 
-func CdTotalLaje (pplaje, ppcontrapiso, ppcemamica, sobrecarga float64) float64 {
+func CdTotalLaje(pplaje, ppcontrapiso, ppcemamica, sobrecarga float64) float64 {
 	Pp := pplaje + ppcontrapiso + ppcemamica
-	result := 1.4 * Pp + 1.5 * sobrecarga
+	result := 1.4*Pp + 1.5*sobrecarga
 
 	return result
 }
